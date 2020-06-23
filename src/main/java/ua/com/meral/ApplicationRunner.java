1cfd98c321a14ce1662f8a9977532fbd491faabf
Anton Shliakhtin <antoxashow@gmail.com>
2020-06-23 14:48:00 +0300

    added image for application, added AppConstant class

--- src/main/java/ua/com/meral/ApplicationRunner.java
+++ src/main/java/ua/com/meral/ApplicationRunner.java
@@ -1,7 +1,11 @@
 package ua.com.meral;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import ua.com.meral.constant.AppConstant;
+
+import java.io.InputStream;
 
 public class ApplicationRunner extends Application {
 
@@ -11,6 +15,13 @@ public class ApplicationRunner extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("JavaFX Test Application");
+        primaryStage.getIcons().add(getImage(AppConstant.MERAL_IMAGE));
         primaryStage.show();
+    }
+
+    private Image getImage(String file) {
+        InputStream iconStream = getClass().getResourceAsStream(AppConstant.IMAGES_PATH + file);
+        return new Image(iconStream);
     }
 }
---
+++ src/main/java/ua/com/meral/constant/AppConstant.java
@@ -0,0 +1,12 @@
+package ua.com.meral.constant;
+
+public final class AppConstant {
+
+    public static final String IMAGES_PATH = "/img/";
+
+    public static final String MERAL_IMAGE = "meral.png";
+
+    private AppConstant() {
+        throw new UnsupportedOperationException("Cannot create instance of AppConstant.class");
+    }
+}
