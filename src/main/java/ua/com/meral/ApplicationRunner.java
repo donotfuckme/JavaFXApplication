a1c759eb00fe8b982a8789a46938c96d5afed641
Anton Shliakhtin <antoxashow@gmail.com>
2020-06-23 15:10:39 +0300

    added scene loader, added mainScene.fxml

--- src/main/java/ua/com/meral/ApplicationRunner.java
+++ src/main/java/ua/com/meral/ApplicationRunner.java
@@ -1,11 +1,16 @@
 package ua.com.meral;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import ua.com.meral.constant.AppConstant;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
 public class ApplicationRunner extends Application {
 
@@ -17,7 +22,22 @@ public class ApplicationRunner extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle(AppConstant.APPLICATION_TITLE);
         primaryStage.getIcons().add(getImage(AppConstant.MERAL_IMAGE));
+
+        primaryStage.setWidth(AppConstant.APP_WIDTH);
+        primaryStage.setHeight(AppConstant.APP_HEIGHT);
+
+        primaryStage.setScene(loadScene(AppConstant.FXML.MAIN_SCENE));
+
         primaryStage.show();
+    }
+
+    private Scene loadScene(String resourceName) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource(AppConstant.FXML_PATH + resourceName);
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+
+        return new Scene(root);
     }
 
     private Image getImage(String file) {
--- src/main/java/ua/com/meral/constant/AppConstant.java
+++ src/main/java/ua/com/meral/constant/AppConstant.java
@@ -4,11 +4,26 @@ public final class AppConstant {
 
     public static final String APPLICATION_TITLE = "JavaFX Test Application";
 
-    public static final String IMAGES_PATH = "/img/";
+    public static final int APP_WIDTH = 600;
+
+    public static final int APP_HEIGHT = 400;
 
     public static final String MERAL_IMAGE = "meral.png";
 
+    public static final String FXML_PATH = "/fxml/";
+
+    public static final String IMAGES_PATH = "/img/";
+
     private AppConstant() {
         throw new UnsupportedOperationException("Cannot create instance of AppConstant.class");
+    }
+
+    public final static class FXML {
+
+        public static final String MAIN_SCENE = "mainScene.fxml";
+
+        private FXML() {
+            throw new UnsupportedOperationException("Cannot create instance of FXML.class");
+        }
     }
 }
